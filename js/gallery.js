1e7160e14ad58fb1858e37db9b554a416ce44c38
MarwR <marw.rapti@gmail.com>
2017-05-06 15:55:30 +0300

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+		//debugger;
+		jQuery(document).ready(function(){	
+			jQuery('.photo-thumbnails .thumbnail').click(function() {
+				jQuery('.photo-thumbnails .thumbnail').removeClass('current');
+				jQuery(this).addClass('current');
+				var path = jQuery(this).find('img').attr('src');
+				jQuery('#big-photo img').attr('src', path);
+			});
+			  
+		});  
